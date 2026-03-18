5ab4789a12856941084d48eb89d078a41b20e87d
obt <byeongtaek12@naver.com>
2026-03-18 13:04:44 +0900

    feat: validation 추가
    - 프론트 단에서는 필수적으로 필요하다고 required를 붙였는데, 백엔드 단에는 붙이지 않아서 우회해서 접근했을 때 방어하기 위함

--- src/main/java/com/example/clothsdanawa/auth/dto/AuthSignUpRequestDto.java
+++ src/main/java/com/example/clothsdanawa/auth/dto/AuthSignUpRequestDto.java
@@ -1,16 +1,22 @@
 package com.example.clothsdanawa.auth.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class AuthSignUpRequestDto {
+	@NotBlank(message = "이름은 필수입니다")
 	private String name;
 	@Email
+	@NotBlank(message = "이메일은 필수입니다")
 	private String email;
+	@NotBlank(message = "패스워드는 필수입니다")
 	private String password;
+	@NotBlank(message = "주소는 필수입니다")
 	private String address;
+	@NotBlank(message = "역할은 필수입니다")
 	private String userRole;
 }
