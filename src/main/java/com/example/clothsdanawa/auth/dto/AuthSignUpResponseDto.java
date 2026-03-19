46fe4bf82c5eda8e9cea98d1accd6a0ed2d5719c
obt <byeongtaek12@naver.com>
2026-03-19 11:36:31 +0900

    feat: 회원가입 dto 생성
    - 로그인 dto와 회원가입 dto 분리를 위함

---
+++ src/main/java/com/example/clothsdanawa/auth/dto/AuthSignUpResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.clothsdanawa.auth.dto;
+
+import com.example.clothsdanawa.user.entity.User;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AuthSignUpResponseDto {
+	private Long id;
+
+	public static AuthSignUpResponseDto from(User user) {
+		return AuthSignUpResponseDto.builder()
+			.id(user.getUserId())
+			.build();
+	}
+}
